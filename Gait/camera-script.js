2a3ece9dfb4aa5fae8301279a7cad90de5536a1a
Ashish Gupta <ashishgupta5096@gmail.com>
2020-10-08 20:59:01 +0530

    Update camera-script.js

--- Gait/camera-script.js
+++ Gait/camera-script.js
@@ -2,38 +2,89 @@
 const canvas =  document.getElementById("pose-canvas");
 const ctx = canvas.getContext("2d");
 const video = document.getElementById("pose-video");
-let hflag =false, kflag=false, strideflag=false, rsflag=false, lsflag=false;
+
+var dflag=false, kflag=false, strideflag=false, rsflag=false, lsflag=false;
+var lr_step_threshold=0, height_px=0, height_cm;
+var  n1=0, n2=0, cnfThreshold=0.10;
+
 let pose;
-var d1=0,d2=0;
+
 
 const config ={
     video:{ width: 640, height: 480, fps: 30}
 };
 
 
-function toggleHeight(button)
+/*function download(content, fileName, contentType) {
+    var a = document.createElement("a");
+    var file = new Blob([content], {type: contentType});
+    a.href = URL.createObjectURL(file);
+    a.download = fileName;
+    a.click();
+}*/
+
+
+async function initializeParameters(button)
 {
-    if (hflag) 
+    let eyeL = pose.pose.leftEye
+    let eyeR = pose.pose.rightEye
+    let ankleL = pose.pose.leftAnkle
+    let ankleR = pose.pose.rightAnkle
+    let count =0;
+    let timeFrame = 2000
+    let start = new Date().getTime();
+    let end = start;
+    
+    while(end - start < timeFrame)
     {
-        hflag = false;
-        button.innerHTML= "Detect"; 
+        if(eyeL.confidence >= cnfThreshold && eyeR.confidence >= cnfThreshold && ankleL.confidence >= cnfThreshold && ankleR.confidence>=cnfThreshold)
+        {
+            height_px = height_px+ (distance(0, eyeL.y, 0, ankleL.y) + distance(0, eyeR.y, 0, ankleR.y))/2
+            lr_step_threshold = lr_step_threshold + distance(0, ankleL.y, 0, ankleR.y)
+            count = count +1;
+            button.innerHTML = "Initializing ...."
+        }
+
+        end = new Date().getTime();
+    }
+        
+
+    height_cm = document.getElementById("height").value;
+    height_px  = (height_px / count).toFixed(2);
+    lr_step_threshold = ((lr_step_threshold/count) * (height_cm / height_px)).toFixed(2)
+
+    button.innerHTML = "Done"
+}
+
+function toggleDistance(button)
+{
+    if (dflag)
+    {
+        dflag = false;
+        button.innerHTML= "Start"; 
     } 
     else 
     {
-        hflag = true;
-        button.innerHTML= "Pause";
+        dflag = true;
+        button.innerHTML= "Stop";
     }
 }
 
+/*async function timer()
+{
+
+}*/
+
 function toggleKnee(button)
 {
-    if (kflag) 
+    if (kflag)
     {
         kflag = false;
         button.innerHTML= "Detect"; 
     } 
     else 
     {
+        
         kflag = true;
         button.innerHTML= "Pause";
     }
@@ -46,10 +97,18 @@ function toggleStrideLength(button)
         strideflag = false;
         button.innerHTML= "Detect"; 
     } 
-    else 
-    {
-        strideflag = true;
-        button.innerHTML= "Pause";
+    else {
+
+        if(!rsflag){
+            document.getElementById("stride").innerHTML= "Activate Right Step Length"
+        }
+        else if(!lsflag){
+            document.getElementById("stride").innerHTML = "Activate Left Step Length"
+        }
+        else{
+            strideflag = true;
+            button.innerHTML= "Pause";
+        }   
     }
 }
 
@@ -58,7 +117,9 @@ function toggleRightStep(button)
     if (rsflag) 
     {
         rsflag = false;
-        button.innerHTML= "Detect"; 
+        button.innerHTML= "Detect";
+        let data = JSON.stringify(rightData, null, 2);
+        download(data, 'right.json', 'text/plain');
     } 
     else 
     {
@@ -72,18 +133,15 @@ function toggleLeftStep(button)
     if (lsflag) 
     {
         lsflag = false;
-        button.innerHTML= "Detect"; 
+        button.innerHTML= "Detect";
+        let data = JSON.stringify(leftData, null, 2);
+        download(data, 'left.json', 'text/plain');
     } 
     else 
     {
         lsflag = true;
         button.innerHTML= "Pause";
     }
-}
-
-function modelReady()
-{
-    console.log('Model Ready')
 }
 
 function distance(x1,y1,x2,y2)
@@ -113,6 +171,62 @@ function drawLine(x1,y1,x2,y2,color)
     ctx.stroke();
 }
 
+function modelReady()
+{
+    console.log('Model Ready')
+    video.play();
+    draw();
+}
+
+async function main()
+{
+    const initializeBttn = document.getElementById("bttn3");
+    const strideBttn = document.getElementById("bttn4");
+    const rightStepBttn = document.getElementById("bttn5");
+    const leftStepBttn = document.getElementById("bttn6");
+    const kneeBttn = document.getElementById("bttn7")
+    const distanceBttn = document.getElementById("bttn8");
+
+    initializeBttn.onclick = function(){
+        initializeParameters(initializeBttn)
+    }
+
+    strideBttn.onclick = function(){
+        toggleStrideLength(strideBttn)
+    }
+
+    rightStepBttn.onclick = function(){
+        toggleRightStep(rightStepBttn)
+    }
+
+    leftStepBttn.onclick = function(){
+        toggleLeftStep(leftStepBttn)
+    }
+
+    kneeBttn.onclick = function(){
+        toggleKnee(kneeBttn)
+    }
+
+    distanceBttn.onclick = function(){
+        toggleDistance(distanceBttn)
+    }
+
+    const options = {
+        imageScaleFactor: 0.3,
+        outputStride: 16,
+        flipHorizontal: false,
+        minConfidence: 0.5,
+        maxPoseDetections: 5,
+        scoreThreshold: 0.5,
+        nmsRadius: 20,
+        detectionType: 'multiple',
+        multiplier: 0.75,
+    }
+
+    const poseNet = ml5.poseNet(video,options, modelReady);
+    poseNet.on('pose',gotPoses);
+}
+
 function gotPoses(poses)
 {
     //console.log(poses)
@@ -121,6 +235,7 @@ function gotPoses(poses)
         pose = poses[0]
     }
 }
+
 
 function draw()
 {
@@ -147,47 +262,72 @@ function draw()
             drawLine(partA.position.x, partA.position.y, partB.position.x, partB.position.y,'red')
         }
 
-        let eyeL = pose.pose.leftEye
-        let eyeR = pose.pose.rightEye
+        //let eyeL = pose.pose.leftEye
+        //let eyeR = pose.pose.rightEye
+
         let ankleL = pose.pose.leftAnkle
         let ankleR = pose.pose.rightAnkle
         let kneeL = pose.pose.leftKnee
         let kneeR = pose.pose.rightKnee
 
-        if (hflag == true)
+        //drawPoint(kneeL.x,kneeL.y,5,'blue')
+        //document.getElementById("test1").innerHTML = (ankleR.x - ankleL.x).toFixed(2)
+        //document.getElementById("test2").innerHTML = (ankleR.y - ankleL.y).toFixed(2)
+
+        if(dflag)
         {
-            var d = distance(eyeL.x, eyeL.y, ankleL.x, ankleL.y)
-            document.getElementById("height").innerHTML= d.toFixed(2);
+            let d = config.video.height-(ankleL.y+ankleR.y)/2
+            d = (height_cm / height_px) * d
+            document.getElementById("distance").innerHTML = d.toFixed(2)
         }
 
         if(kflag == true)
         {
-            var d = distance(kneeL.x, kneeL.y, kneeR.x, kneeR.y)
+            let d = distance(kneeL.x, kneeL.y, kneeR.x, kneeR.y)
+            d = (height_cm / height_px) * d
             document.getElementById("knee-d").innerHTML= d.toFixed(2);
         }
 
         if(rsflag == true)
         {
-            var d = distance(ankleL.x, ankleL.y, ankleR.x, ankleR.y)
-            document.getElementById("rs-d").innerHTML = d.toFixed(2);
-            d1=d;
+            let d = ankleR.y - ankleL.y
+            d = (height_cm / height_px) * d
+
+            if (d <= lr_step_threshold)
+            {
+                document.getElementById("rs-d").innerHTML = 0;
+            }
+            else
+            {
+                document.getElementById("rs-d").innerHTML = d.toFixed(2);
+                n1=d;
+            } 
         }
 
         if(lsflag == true)
         {
-            var d = distance(ankleL.x, ankleL.y, ankleR.x, ankleR.y)
-            document.getElementById("ls-d").innerHTML = d.toFixed(2)
-            d2=d;
+            let d = ankleL.y - ankleR.y
+            d = (height_cm / height_px) * d
+
+            if (d <= lr_step_threshold)
+            {
+                document.getElementById("ls-d").innerHTML = 0;
+            }
+            else
+            {
+                document.getElementById("ls-d").innerHTML = d.toFixed(2);
+                n2=d;
+            } 
         }
 
         if(strideflag == true)
         {
-            if( (d1+d2) >0 )
+            if( n1 >= 0 && n2 >= 0 )
                 document.getElementById("stride").innerHTML = (d1+d2).toFixed(2)
             else
                 document.getElementById("stride").innerHTML = "Unable to detect feet";
-
         }
+
     }
     
     requestAnimationFrame(draw);
@@ -195,63 +335,6 @@ function draw()
 }
 
 
-async function main()
-{
-    const startBttn = document.getElementById("start");
-    const stopBttn = document.getElementById("stop");
-    const heightBttn = document.getElementById("bttn1");
-    const strideBttn = document.getElementById("bttn2");
-    const rightStepBttn = document.getElementById("bttn3");
-    const leftStepBttn = document.getElementById("bttn4");
-    const kneeBttn = document.getElementById("bttn5")
-
-    heightBttn.onclick = function(){
-        toggleHeight(heightBttn)
-    }
-
-    strideBttn.onclick = function(){
-        toggleStrideLength(strideBttn)
-    }
-
-    rightStepBttn.onclick = function(){
-        toggleRightStep(rightStepBttn)
-    }
-
-    leftStepBttn.onclick = function(){
-        toggleLeftStep(leftStepBttn)
-    }
-
-    kneeBttn.onclick = function(){
-        toggleKnee(kneeBttn)
-    }
-
-    const options = {
-        imageScaleFactor: 0.3,
-        outputStride: 16,
-        flipHorizontal: false,
-        minConfidence: 0.5,
-        maxPoseDetections: 5,
-        scoreThreshold: 0.5,
-        nmsRadius: 20,
-        detectionType: 'multiple',
-        multiplier: 0.75,
-    }
-
-    const poseNet = ml5.poseNet(video,options, modelReady);
-    poseNet.on('pose',gotPoses);
-
-    startBttn.onclick = function(){
-        console.log("loadeddata");
-        video.play();
-        //setTimeout(videoLoop, 1000 / 30);
-        draw()
-    }
-
-    stopBttn.onclick = function(){
-        video.pause()
-    }
-
-}
 
 async function init_camera_canvas()
 {
